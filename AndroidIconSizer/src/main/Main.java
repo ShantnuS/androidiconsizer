b5babb68d2df1828e1a48aed282b22edae20f4eb
ShantnuS <shantnu@outlook.com>
2017-08-01 11:48:51 +0530

    Added initial java files

---
+++ AndroidIconSizer/src/main/Main.java
@@ -0,0 +1,56 @@
+package main;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class Main {
+
+	
+	//Converts Image to BufferedImage
+	public static BufferedImage toBufferedImage(Image img)
+	{
+	    if (img instanceof BufferedImage)
+	    {
+	        return (BufferedImage) img;
+	    }
+
+	    // Create a buffered image with transparency
+	    BufferedImage bimage = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+
+	    // Draw the image on to the buffered image
+	    Graphics2D bGr = bimage.createGraphics();
+	    bGr.drawImage(img, 0, 0, null);
+	    bGr.dispose();
+
+	    // Return the buffered image
+	    return bimage;
+	}
+	
+	public static void main(String[] args){
+		BufferedImage image = null;
+		
+		try {
+		    image = ImageIO.read(new File("C:/Users/Shantnu/Desktop/image/ml_icon512.png")); // eventually C:\\ImageTest\\pic2.jpg
+		} 
+		catch (IOException e) {
+		    e.printStackTrace();
+		}
+		
+		Image image2 = image.getScaledInstance(192, 192, Image.SCALE_SMOOTH);
+		BufferedImage img = Main.toBufferedImage(image2);
+		
+		try {
+		    // retrieve image
+		    File outputfile = new File("C:/Users/Shantnu/Desktop/image/ml_icon192.png");
+		    ImageIO.write(img, "png", outputfile);
+		} 
+		catch (IOException e) {
+		    e.printStackTrace();
+		}
+	}
+	
+}
