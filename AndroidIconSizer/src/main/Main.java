ed4c2acd032fb8a9418129f0b6199fce022b68a6
ShantnuS <shantnu@outlook.com>
2017-08-02 11:08:35 +0530

    v1 of program

--- AndroidIconSizer/src/main/Main.java
+++ AndroidIconSizer/src/main/Main.java
@@ -2,12 +2,21 @@ package main;
 
 import ui.ResizeFrame;
 
+/*
+ * Author: ShantnuS
+ * Version: 1.0
+ * Date: 02/08/2017
+ * 
+ * ABOUT: Android Icon Sizer automatically creates the multiple resolutions needed for icons
+ * on android and puts them in the correct folders. It automates the previously annoying procedure
+ * of having to do it manually. 
+ */
+
 public class Main {
 	
 	public static void main(String[] args){
 		ResizeFrame resizer = new ResizeFrame();
 		resizer.init();
-		
 	}
 	
 }
